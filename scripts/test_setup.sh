2c19d1a9d00fe0da76f0c45ab53916a591008031
Stephen Couchman <lanfear@greyscribes.com>
2015-10-29 23:16:50 -0400

    Further tweaking setup script to try to grant correct permissions for PG test.

--- scripts/test_setup.sh
+++ scripts/test_setup.sh
@@ -3,19 +3,21 @@
 echo Setting up PostgreSQL test environment...
 
 psql -U postgres << DONESQL
-	CREATE DATABASE play_driver_test_alpha;
+	CREATE USER alphauser WITH PASSWORD 'testpass';
+	CREATE USER betauser WITH PASSWORD 'testpass';
+	CREATE DATABASE play_driver_test_alpha WITH OWNER alphauser;
 	\connect play_driver_test_alpha
 	CREATE TABLE magic_numbers (
 		num INTEGER NOT NULL PRIMARY KEY
 	);
-	CREATE DATABASE play_driver_test_beta;
+	ALTER TABLE magic_numbers OWNER TO alphauser;
+	CREATE DATABASE play_driver_test_beta WITH OWNER betauser;
 	\connect play_driver_test_beta
 	CREATE TABLE magic_colors (
 		color varchar PRIMARY KEY
 	);
-	CREATE USER alphauser WITH PASSWORD 'testpass';
+	ALTER TABLE magic_colors OWNER TO betauser;
 	GRANT ALL PRIVILEGES ON DATABASE play_driver_test_alpha TO alphauser;
-	CREATE USER betauser WITH PASSWORD 'testpass';
 	GRANT ALL PRIVILEGES ON DATABASE play_driver_test_beta TO betauser;
 DONESQL
 
