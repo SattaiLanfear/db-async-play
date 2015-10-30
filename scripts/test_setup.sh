4ade59978d22115206aa7277150c66e24c18148e
Stephen Couchman <lanfear@greyscribes.com>
2015-10-29 23:07:30 -0400

    Fixing bad setup script, tables in wrong db.

--- scripts/test_setup.sh
+++ scripts/test_setup.sh
@@ -4,10 +4,12 @@ echo Setting up PostgreSQL test environment...
 
 psql -U postgres << DONESQL
 	CREATE DATABASE play_driver_test_alpha;
+	\connect play_driver_test_alpha
 	CREATE TABLE magic_numbers (
 		num INTEGER NOT NULL PRIMARY KEY
 	);
 	CREATE DATABASE play_driver_test_beta;
+	\connect play_driver_test_beta
 	CREATE TABLE magic_colors (
 		color varchar PRIMARY KEY
 	);
