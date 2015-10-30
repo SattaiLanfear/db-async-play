e0a179b8786371ab12fa12c2eb7a226d5c4854a9
Stephen Couchman <lanfear@greyscribes.com>
2015-10-30 08:33:32 -0400

    First-pass at adding MySQL test setup.

--- scripts/test_setup.sh
+++ scripts/test_setup.sh
@@ -14,11 +14,32 @@ psql -U postgres << DONESQL
 	CREATE DATABASE play_driver_test_beta WITH OWNER betauser;
 	\connect play_driver_test_beta
 	CREATE TABLE magic_colors (
-		color varchar PRIMARY KEY
+		color varchar NOT NULL PRIMARY KEY
 	);
 	ALTER TABLE magic_colors OWNER TO betauser;
 	GRANT ALL PRIVILEGES ON DATABASE play_driver_test_alpha TO alphauser;
 	GRANT ALL PRIVILEGES ON DATABASE play_driver_test_beta TO betauser;
 DONESQL
 
-echo PostgreSQL setup done..
+echo PostgreSQL setup done...
+
+echo Setting up MySQL test environment...
+
+mysql -u root << DONESQL
+	CREATE DATABASE play_driver_test_alpha;
+	CREATE TABLE IF NOT EXISTS play_driver_test_alpha.magic_numbers (
+        num INT NOT NULL,
+        PRIMARY KEY (num)
+    );
+	CREATE DATABASE play_driver_test_beta;
+	CREATE TABLE IF NOT EXISTS play_driver_test_beta.magic_colors (
+		color VARCHAR(128) NOT NULL,
+		PRIMARY KEY (color)
+	);
+	GRANT ALL PRIVILEGES ON play_driver_test_alpha.* TO 'alphauser'@'localhost' IDENTIFIED BY 'testpass' WITH GRANT OPTION;
+	GRANT ALL PRIVILEGES ON play_driver_test_beta.* TO 'betauser'@'localhost' IDENTIFIED BY 'testpass' WITH GRANT OPTION;
+DONESQL
+
+echo MySQL setup done...
+
+
